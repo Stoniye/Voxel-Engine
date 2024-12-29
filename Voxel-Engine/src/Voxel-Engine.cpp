83498866efb0e68d88804f85de288ab8b017e95e
Stonie <aronstein10@gmail.com>
2024-12-29 21:00:43 +0100

    Draw Triangle with OpenGL
    Draw Triangle with modern OpenGL and not with Legacy OpenGL

--- Voxel-Engine/src/Voxel-Engine.cpp
+++ Voxel-Engine/src/Voxel-Engine.cpp
@@ -21,14 +21,30 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    /* Initialize glew and test for errors */
     if (glewInit() != GLEW_OK)
         std::cout << "GLEW Error" << std::endl;
+
+    float positions[6] = {
+        -0.5f, -0.5f,
+        0.0f, 0.5f,
+        0.5f, -0.5f
+    };
+
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
