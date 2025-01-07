aacdbfec43a77686ad824005195baf87253aaa67
Stonie <aronstein10@gmail.com>
2025-01-07 21:13:55 +0100

    Abstract Rederer, Vertex-, IndexBuffer
    - Abstracted the Renderer and VertexBuffer and IndexBuffer to .h and .cpp file for better overview

--- Voxel-Engine/src/Voxel-Engine.cpp
+++ Voxel-Engine/src/Voxel-Engine.cpp
@@ -1,27 +1,17 @@
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
+//Inlcude OpenGL
+#include "GL/glew.h"
+#include "GLFW/glfw3.h"
 
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <sstream>
+//Include System Things
+#include "iostream"
+#include "fstream"
+#include "string"
+#include "sstream"
 
-#define ASSERT(x) if (!(x)) __debugbreak();
-#define GLCall(x) GLClearError();x;ASSERT(GLLogCall(#x, __FILE__, __LINE__))
-
-static void GLClearError()
-{
-    while (glGetError() != GL_NO_ERROR);
-}
-
-static bool GLLogCall(const char* function, const char* file, int line)
-{
-    while (GLenum error = glGetError()) {
-        std::cout << "[OpenGL Erorr] Error Code: " << error << " | " << function << " | " << file << " | Line " << line << std::endl;
-        return false;
-    }
-    return true;
-}
+//Include Abstracts
+#include "Renderer.h"
+#include "VertexBuffer.h"
+#include "IndexBuffer.h"
 
 struct ShaderProgramSource
 {
@@ -130,43 +120,28 @@ int main(void)
         2, 3, 0
     };
 
-    unsigned int buffer;
-    glGenBuffers(1, &buffer);
-    glBindBuffer(GL_ARRAY_BUFFER, buffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW);
 
+    VertexBuffer vb(positions, sizeof(positions));
+    
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
 
-    unsigned int indexBuffer;
-    glGenBuffers(1, &indexBuffer);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
+    IndexBuffer ib(indices, 6);
     
     ShaderProgramSource source = ParseShader("res/shaders/Basic.shader");
     unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
     glUseProgram(shader);
 
     int location = glGetUniformLocation(shader, "u_Color");
+    glUniform4f(location, 0.0f, 1.0f, 0.0f, 1.0f);
 
-    float r = 0.0f;
-    float increment = 0.05f;
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glUniform4f(location, r, 0.3f, 0.8f, 1.0f);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
-
-        if (r > 1.0f)
-            increment = -0.05f;
-        else if (r < 0.0f)
-            increment = 0.05f;
-
-        r += increment;
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
