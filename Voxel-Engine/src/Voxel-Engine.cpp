dedd471a555f5d78d1358428439ccf19916d6c9c
Stonie <aronstein10@gmail.com>
2025-01-07 20:05:17 +0100

    Uniforms in Shader
    - Made Disco Rectangle with uniforms used in the shader

--- Voxel-Engine/src/Voxel-Engine.cpp
+++ Voxel-Engine/src/Voxel-Engine.cpp
@@ -112,6 +112,8 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    glfwSwapInterval(5);
+
     /* Initialize glew and test for errors */
     if (glewInit() != GLEW_OK)
         std::cout << "GLEW Error" << std::endl;
@@ -146,13 +148,25 @@ int main(void)
     unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
     glUseProgram(shader);
 
+    int location = glGetUniformLocation(shader, "u_Color");
+
+    float r = 0.0f;
+    float increment = 0.05f;
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
+        glUniform4f(location, r, 0.3f, 0.8f, 1.0f);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+
+        if (r > 1.0f)
+            increment = -0.05f;
+        else if (r < 0.0f)
+            increment = 0.05f;
+
+        r += increment;
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
